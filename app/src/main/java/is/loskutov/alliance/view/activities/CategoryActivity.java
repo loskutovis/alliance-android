be78bf570c7e6046e60cd829bf5dfafd19e25bad
Игорь Лоскутов <loskutovis@gmail.com>
2017-09-21 22:49:06 +0300

    Правки

--- app/src/main/java/is/loskutov/alliance/view/activities/CategoryActivity.java
+++ app/src/main/java/is/loskutov/alliance/view/activities/CategoryActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
